f4d713d30986dbc3b9db0f645614016c59c2c1f0
Pedro <pedro.lavado.contactor@bbva.com>
2024-02-23 11:40:17 +0100

    fix text

--- src/common/constant.js
+++ src/common/constant.js
@@ -1,8 +1,8 @@
 export const constant = {
   LEVEL_GAME: {
-    LOW: { POINTS: 10, TIME: 10000, TEXT: "Low" },
-    MEDIUM: { POINTS: 20, TIME: 5000, TEXT: "Medium" },
-    HIGH: { POINTS: 30, TIME: 2000, TEXT: "High" },
+    LOW: { POINTS: 10, TIME: 10000, TEXT: "LOW" },
+    MEDIUM: { POINTS: 20, TIME: 5000, TEXT: "MEDIUM" },
+    HIGH: { POINTS: 30, TIME: 2000, TEXT: "HIGH" },
   },
   LIST_LEVELS: ["LOW", "MEDIUM", "HIGH"],
   SECOND: 1000,
